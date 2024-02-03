e0fcddd6a18690c0f89150e974a948b4d1e1a902
Jacobo Rodicio González <jacobo.rodicio@it-manufactory.com>
2024-02-03 11:14:24 +0100

    feat: java support & avoid overriding register when paste on selection

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,3 +26,7 @@ keymap("n", "<C-d>", "-17<C-d>zz", opts)
 -- Press jk or JK fast to escape
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "JK", "<ESC>", opts)
+
+-- VISUAL --
+-- Paste without overwriting the current register
+keymap("v", "p", "pgvy", opts)
