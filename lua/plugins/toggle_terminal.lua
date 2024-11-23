a234366a6860789bcc4ba24eac631ee71aaadd9a
Jacobo Rodicio González <jacobo.rodicio@it-manufactory.com>
2024-11-23 21:45:10 +0100

    fix: Terminal size

--- lua/plugins/toggle_terminal.lua
+++ lua/plugins/toggle_terminal.lua
@@ -3,12 +3,32 @@ return {
   version = "*",
   config = function()
     require("toggleterm").setup({
-      size = 20, -- Tamaño de la terminal
-      open_mapping = [[<C-->]], -- Aquí puedes usar <C--> o cualquier combinación
-      direction = "float", -- Terminal flotante
+      size = function(term)
+        -- Si la dirección es "horizontal", usa el 100% del alto
+        if term.direction == "horizontal" then
+          return vim.o.lines
+        elseif term.direction == "vertical" then
+          return vim.o.columns
+        end
+      end,
+      open_mapping = [[<C-->]], -- Ajusta la combinación de teclas según tu preferencia
+      direction = "float", -- Terminal en modo flotante
       float_opts = {
-        border = "curved", -- Bordes agradables
+        border = "single", -- Opciones: 'single', 'double', 'shadow', 'curved', 'none'
+        width = function()
+          return vim.o.columns - 5
+        end, -- Ancho: toda la pantalla
+        height = function()
+          return vim.o.lines - 5
+        end, -- Alto: toda la pantalla
       },
+      -- Opcional: Ajustar sombreado del fondo
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 1, -- Ajusta el nivel de sombreado del fondo (0-3)
+      persist_size = true,
+      close_on_exit = true, -- Cierra la terminal al salir
+      shell = vim.o.shell, -- Usa la shell predeterminada del sistema
     })
   end,
 }
